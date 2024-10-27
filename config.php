5d20940cb169be228495947114f6e70339eb5539
agstydva <agastyadava01@gmail.com>
2024-10-27 16:44:21 +0700

    file ppw

---
+++ config.php
@@ -0,0 +1,4 @@
+<!-- ---------[UNTUK MENGHUBKAN KE MYSQL]--------- -->
+<?php
+$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+?>
---
+++ header.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <div class="flex">
+
+      <a href="#" class="logo">Dava Shop</a>
+
+      <nav class="navbar">
+         <a href="admin.php">add products</a>
+         <a href="products.php">view products</a>
+      </nav>
+
+      <a href="cart.php" class="cart">cart <span>0</span> </a>
+
+      <div id="menu-btn" class="fas fa-bars"></div> 
+
+   </div>
+</header>
