455954f5eae04dbe118a495133985175e3e093c0
cinkar <147405811+cinkar@users.noreply.github.com>
2024-12-19 11:16:11 +0700

    Update header4.php

--- header4.php
+++ header4.php
@@ -6,6 +6,14 @@
         Dtastyum
       </a>
 
+        <div id="search_">
+         <form action="" method="GET">
+            <input type="text" name="keyword" id="search_bar" placeholder="Search">
+            <button type="submit" name="search"><i class='fas fa-search' style='background-color: white; width: 30px; height: 30px; margin-top: 0px; padding-top: 10px; '></i>
+            </button>
+         </form>
+        </div>
+
       <nav class="navbar">
          <a href="user_page.php">Products</a>
       </nav>
@@ -27,3 +35,23 @@
 
    </div>
 </header>
+
+<style>
+        #search_ {
+            border: 1px solid black;
+            width: 370px;
+            height: 35px;
+            border-radius: 20px;
+            background-color: white;
+            margin-right: 80px;
+        }
+
+        #search_bar {
+            width: 325px;
+            height: 30px;
+            border-radius: 20px;
+            padding-left: 10px;
+            padding-top: 3px;
+            font-size: 18px;
+        }
+    </style>
